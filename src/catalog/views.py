e4a8e25fd23e5b169c565cd87eb1666cfc895979
LyingCat <waltersullivan293@gmail.com>
2021-04-28 17:55:15 +0300

    auth created

--- src/catalog/views.py
+++ src/catalog/views.py
@@ -13,6 +13,8 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
     num_genres = Genre.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
 
     context = {
         'num_books': num_books,
@@ -20,7 +22,8 @@ def index(request):
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genres': num_genres,
-        'num_books_containing_love_in_title': num_books_containing_love_in_title
+        'num_books_containing_love_in_title': num_books_containing_love_in_title,
+        'num_visits': num_visits
     }
 
     return render(request, 'index.html', context)
